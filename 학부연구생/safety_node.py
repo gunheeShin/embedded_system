bcd6753773a48465c7e2385e2bd99e576b96c11f
gunheeShin <96728705+gunheeShin@users.noreply.github.com>
2022-01-12 14:53:48 +0900

    Add files via upload

---
+++ 학부연구생/safety_node.py
@@ -0,0 +1,78 @@
+#------------------------------------------------------
+# File name: safety_node
+# YOUTIBE LINK: https://youtu.be/MwRyheFMkqw
+# Programmed by Gun-Hee Shin on Jan 2022
+#-------------------------------------------------------
+
+
+
+
+#!/usr/bin/env python
+from re import S
+import rospy                                            # rospy is needed to writing ROS node 
+from std_msgs.msg import *                              # to publish message which type is std_msgs.msg/String
+from sensor_msgs.msg import LaserScan                   # to publish message which type is sensor_msgs.msg/LaserScam
+from nav_msgs.msg import Odometry                       # to publish message which type is nav_msgs.msg/Odometry
+from ackermann_msgs.msg import AckermannDriveStamped    # to publish message which type is geometry_msg/AckermannderiveStamped
+from math import *                                      # math is needed to use calculation function and (pi,e)                             
+
+class Safety(object): #The class that handles emergency braking.
+
+    def __init__(self):
+        
+        # node subscribes to the odom topic which is of type nav_msgs/Odometry. 
+        rospy.Subscriber("odom", Odometry, self.odom_callback) 
+        # node subscribes to the scan topic which is of type sensor_msgs/LaserScam. 
+        rospy.Subscriber("scan", LaserScan, self.scan_callback) #receive 'scan' topic
+        
+        # node is publishing to the brake_bool topic using the message type Bool
+        self.pub_brake_bool= rospy.Publisher("brake_bool", Bool, queue_size=10)
+        # node is publishing to the /brake topic using the message type AckermannDrivestamped
+        self.pub_brake = rospy.Publisher("/brake", AckermannDriveStamped, queue_size=10)
+        
+        self.threshold = 0.3
+        self.breakspeed = 0
+        
+
+
+    def odom_callback(self, odom_msg):
+        self.odom = odom_msg # receive odom_msg        
+
+    def scan_callback(self, scan_msg):
+        
+        self.scan=scan_msg   # receive scan_msg
+        
+        angle_min=scan_msg.angle_min                # Lidar minumum angle: -2.355rad
+        angle_increment = scan_msg.angle_increment  # Laser angle increment: 4.71/1080[rad], scan beams:1080
+        ranges = scan_msg.ranges                    # Distance between vehicle and obstacle
+        velocity = self.odom.twist.twist.linear.x   # vehicle velocity
+       
+        brake_bool_msg=Bool()                       # message which type is Bool
+        brake_bool_msg.data=False                   # update parameter 'data' in brake_bool_msg as False
+        
+        brake_msg=AckermannDriveStamped()           # message which type is Bool
+        brake_msg.drive.speed=self.breakspeed            # update parameter 'data' in brake_bool_msg as False
+        
+        
+        for i in range(len(ranges)):                # for every laser point
+            theta = angle_min + angle_increment * i # calculate theta based on angle_min(-2.355rad)
+            
+            if velocity != 0:                       
+                velocity = abs(velocity)                            # to be robust from noise(negative)
+                if -pi/10 < theta < pi/10:                          # set the FOV(Field of View)
+                    TTC = ranges[i] / (velocity*cos(theta))         # Cacluate TTC
+                    if TTC < self.threshold:                        # if TTC is smaller than threshold, then vehicle must be stopped
+                        brake_bool_msg.data=True                    # update parameter 'data' in brake_bool_msg as False
+                        self.pub_brake_bool.publish(brake_bool_msg) # publish brake_bool_msg
+                        self.pub_brake.publish(brake_msg)           # publish brake_msg
+                                     
+        brake_bool_msg.data=False   
+        self.pub_brake_bool.publish(brake_bool_msg)
+        
+       
+def main():
+    rospy.init_node('safety_node')
+    sn = Safety()
+    rospy.spin()
+if __name__ == '__main__':
+    main()
---
+++ 학부연구생/wall_follow.py
@@ -0,0 +1,108 @@
+#------------------------------------------------------
+# File name: wall_follow
+# YOUTIBE LINK: https://youtu.be/vpMMhKbd5ac
+# Programmed by Gun-Hee Shin on Jan 2022
+#-------------------------------------------------------
+
+
+
+#!/usr/bin/env python
+from __future__ import print_function
+import sys
+from math import *
+import numpy as np
+
+#ROS Imports
+import rospy
+from sensor_msgs.msg import Image, LaserScan                            # to publish message which type is sensor_msgs/(Image,LaserScan)
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive    # to publish message which type is ackermann_msg/(AckermannderiveStamped,AckermannDrive)
+
+kp = 14    
+kd = 0.09   
+ki = 0      
+servo_offset = 0.0
+prev_error = 0.0 
+error = 0.0
+integral = 0.0
+
+#WALL FOLLOW PARAMS
+ANGLE_RANGE = 270               # Hokuyo 10LX has 270 degrees scan
+DESIRED_DISTANCE_RIGHT = 0.9    # desired distance from right wall
+DESIRED_DISTANCE_LEFT = 0.8     # desired distance from left wall
+VELOCITY = 1.00                 # vehicle velocity [m/s]
+CAR_LENGTH = 0.50 
+THETA = pi/10
+
+class WallFollow:
+    
+    def __init__(self):
+        
+        lidarscan_topic = '/scan'
+        drive_topic = '/nav'
+
+        # node subscribes to the scan topic which is of type sensor_msgs/LaserScan.
+        self.lidar_sub = rospy.Subscriber(lidarscan_topic, LaserScan, self.lidar_callback)
+        # node is publishing to the /brake topic using the message type AckermannDrivestamped
+        self.drive_pub = rospy.Publisher(drive_topic, AckermannDriveStamped, queue_size=10)
+        
+
+    def lidar_callback(self, data):
+        
+        laser_data = data
+        
+        error = self.followRight(laser_data, DESIRED_DISTANCE_LEFT)
+
+        self.pid_control(error, VELOCITY)   # set the steering angle for the VESC of vehicle using PID Control
+
+
+    def followRight(self, data, RightDist):
+        
+        a, b = self.getRange(data, THETA)               # get the ditstance between vehicle and two random points of right wall 
+        
+        alpha = atan((a*cos(THETA)-b) / a*sin(THETA))   # get the angle between two distance vector
+        
+        D_cur = b*cos(alpha)                            # current distance to wall from ego vehicle
+        D_fut = D_cur + CAR_LENGTH*sin(alpha)           # future distance to wall from ego vehicle
+        
+        caculated_error = D_fut - RightDist              # set error used in PID control
+        return caculated_error
+
+    def getRange(self, data, angle):                    # get the ditstance between vehicle and two random points of right wall 
+        
+        b = data.ranges[269]    
+        
+        i = int(angle / (2*pi/1080))        
+        
+        a = data.ranges[269+i]
+        
+        return a,b 
+     
+    def pid_control(self, error, velocity):
+        global integral
+        global prev_error
+        global kp
+        global ki
+        global kd
+        angle = 0.0
+        
+        angle = kp * error + kd * (error - prev_error)
+        
+        drive_msg = AckermannDriveStamped()         # message which type is AckermannDriveStamped
+        drive_msg.header.frame_id = "laser"         # update parameter frame_id in drive_msg as "laser"
+        drive_msg.drive.steering_angle = -angle     # update parameter steering_angle in drive_msg as -angle
+        drive_msg.drive.speed = velocity            # update parameter speed in drive_msg as velocity
+        
+        self.drive_pub.publish(drive_msg)           # publish drive_msg
+        
+        print(error)
+        prev_error = error                          # set previous error
+
+
+def main(args):
+    rospy.init_node("WallFollow_node", anonymous=True)
+    wf = WallFollow()
+    rospy.sleep(0.1)
+    rospy.spin()
+
+if __name__=='__main__':
+	main(sys.argv)
